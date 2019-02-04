a319f092ad301a8973a5c6a08d2177a0b2c9ee08
yangweike <yangweike@hotmail.com>
2019-02-04 08:46:10 +0800

    init

---
+++ src/component/template/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Stage, Layer, Rect, Text } from 'react-konva'
+import Konva from 'konva'
+
+class Index extends Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
